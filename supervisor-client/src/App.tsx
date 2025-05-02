d49c311c1d953b4347f61f61652c5a5432f1e7f1
Priyanshu Dhall <priyanshudhall2001@gmail.com>
2025-05-03 00:58:29 +0530

    Adding/loading knowledge now works along with styling updates

--- supervisor-client/src/App.tsx
+++ supervisor-client/src/App.tsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import { HomeView } from "./components/HomeView";
 
 function App() {
   return (
-    <>
-      <h1>Supervisor Dashboard</h1>
-    </>
+    <div className="min-h-screen py-8 px-4">
+      <div className="max-w-7xl mx-auto">
+        <h1 className="text-3xl font-medium mb-8">Supervisor Dashboard</h1>
+        <HomeView />
+      </div>
+    </div>
   );
 }
 
